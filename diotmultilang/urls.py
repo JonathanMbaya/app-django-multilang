22aec7919c149d319505a79ab58ab26027e28a0e
unknown <jonathanmbaya13@gmail.com>
2024-06-19 05:35:11 +0200

    Récupération des données Admin des Articles , Page Accueil et Page Blog , lien dynamique pour chaque article

--- diotmultilang/urls.py
+++ diotmultilang/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 app_name = "diotmultilang"
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("blog/", views.articles, name="articles"),
+    path('article/<int:article_id>/', views.articleOne, name='articleOne'),
 ]
--- diotmultilang/views.py
+++ diotmultilang/views.py
@@ -3,12 +3,17 @@ from .models import Article
 
 
 def index(request):
-    # latest_article_list = Article.objects.order_by("-pub_date")[:5]
-    latest_article_list = Article.objects.all()
-    article_list = {"latest_article_list": latest_article_list}
-    return render(request, "diotmultilang/index.html", article_list)
+    latest_article_list = Article.objects.order_by("-publication_date")[:3]
+    article_last = {"latest_article_list": latest_article_list }
+    return render(request, "diotmultilang/index.html", article_last)
 
-# def article(request, article_id):
-#     article = get_object_or_404(Article, pk=article_id)
-#     return render(request, "diotmultilang/article.html", {"article": article})
+def articles(request):
+    all_article_list = Article.objects.all()
+    article_list = {"all_article_list": all_article_list }
+    return render(request, "diotmultilang/blog.html", article_list)
+
+def articleOne(request, article_id):
+    article = get_object_or_404(Article, pk=article_id)
+    return render(request, "diotmultilang/article.html", {"article": article})
+
 
